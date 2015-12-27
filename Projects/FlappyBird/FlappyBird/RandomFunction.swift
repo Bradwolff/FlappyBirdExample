cb3c54369a668c57c61c8b10df78c72ecce2eab4
Bradwolff <s.bradwolff@gmail.com>
2015-12-27 20:56:50 +0100

    init2

---
+++ Projects/FlappyBird/FlappyBird/RandomFunction.swift
@@ -0,0 +1,25 @@
+//
+//  RandomFunction.swift
+//  FlappyBird
+//
+//  Created by Sebastiaan Bradwolff on 24-12-15.
+//  Copyright © 2015 Sebastiaan Bradwolff. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+public extension CGFloat{
+    
+    public static func random() -> CGFloat{
+        
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    }
+    
+    public static func random(min min : CGFloat, max : CGFloat) -> CGFloat{
+    
+        return CGFloat.random() * (max - min) + min
+    }
+    
+}
+
